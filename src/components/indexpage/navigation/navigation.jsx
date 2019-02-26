2f1b6eb6b326f91f2f16e4eee9ff2a8cb1acfcb5
Jakub <jakub@MacBook-Pro-Jakub.local>
2019-02-26 16:54:55 +0100

    Add. navigation for mobile

--- src/components/indexpage/navigation/navigation.jsx
+++ src/components/indexpage/navigation/navigation.jsx
@@ -4,14 +4,31 @@ import { Link } from "gatsby"
 
 
 class navigation extends Component {
+        state = {
+            isOpen: false
+        }
+    
+    toogleMenu = () => {
+       this.setState(prevstate => {
+           return {
+           isOpen: !prevstate.isOpen
+       }})
+    }
     render() {
         return (
-            <ul className="navigation">
-               <li><Link to="#">home</Link></li>
-               <li><Link to="#">o nas</Link></li>
-               <li><Link to="#">nasze produkty</Link></li>
-               <li><Link to="#">kontakt</Link></li>
-            </ul>
+            <div className="navigation-container">
+                <ul className={`navigation ${this.state.isOpen && 'open'}`}>
+                    <li><Link to="#">home</Link></li>
+                    <li><Link to="#">o nas</Link></li>
+                    <li><Link to="#">nasze produkty</Link></li>
+                    <li><Link to="#">kontakt</Link></li>
+                </ul>
+                <div className="hamburger-menu" onClick = {this.toogleMenu}>
+                    <span className="nav-icon-element"></span>
+                    <span className="nav-icon-element"></span>
+                    <span className="nav-icon-element"></span>
+                </div>
+            </div>
         );
     }
 }
